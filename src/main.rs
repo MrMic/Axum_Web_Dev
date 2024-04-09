d9f8f8e1e723f48c36ec1d587cb7bc564e0bd831
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-09 15:56:11 +0200

    ✨ First try with hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)] // For beginning only.
+
+use axum::{response::Html, routing::get, Router, ServiceExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello =
+        Router::new().route("/hello", get(|| async { Html("<strong>Hello World!") }));
+
+    // ╾──────────────────────────╼ START SERVER ╾───────────────────────╼
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    println!("--> lISTENING on {:?}\n", listener.local_addr());
+
+    axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
+
+    // Ok(())
 }
