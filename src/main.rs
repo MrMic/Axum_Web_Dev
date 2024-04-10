6fdfca02236c46f86924e942c19188980cbe0b5c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-10 14:52:03 +0200

    ✨ feat: Implementation tests & watch

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![allow(unused)] // For beginning only.
 
-use axum::{response::Html, routing::get, Router, ServiceExt};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router, ServiceExt,
+};
 use tokio::net::TcpListener;
 
 // ╾──────────────────────────────╼ MAIN ╾───────────────────────────╼
 #[tokio::main]
 async fn main() {
-    let routes_hello =
-        Router::new().route("/hello", get(|| async { Html("<strong>Hello World!") }));
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     // ╾──────────────────────────╼ START SERVER ╾───────────────────────╼
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
-    println!("--> lISTENING on {:?}\n", listener.local_addr());
+    println!("--> LISTENING on {:?}\n", listener.local_addr());
 
     axum::serve(listener, routes_hello.into_make_service())
         .await
         .unwrap();
 
     // Ok(())
+}
+
+// ____________________________ HANDLER_HELLO ____________________________
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello, <strong>World!</strong>")
 }
---
+++ tests/quick_dev.rs
@@ -0,0 +1,12 @@
+#![allow(unused)] // For beginning only.
+
+use anyhow::Result;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
+
+    hc.do_get("/hello").await?.print().await?;
+
+    Ok(())
+}
