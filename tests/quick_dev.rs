cd98b55ea903ccdbae0935a6a0a0ace65934a71b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-11 15:03:16 +0200

    ✨ feat: Path Params for Hello2 implementation

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,9 +4,10 @@ use anyhow::Result;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
-    let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
+	let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
 
-    hc.do_get("/hello?name=Mic").await?.print().await?;
+	// hc.do_get("/hello?name=Mic").await?.print().await?;
+	hc.do_get("/hello2/Mike").await?.print().await?;
 
-    Ok(())
+	Ok(())
 }
