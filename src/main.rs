519d56186fd74e3fd8c7697284f9c2b980a3316d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-12 14:59:45 +0200

    ✨ feat: Add Static Route for "/"

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,43 @@
 use axum::{
 	extract::{Path, Query},
 	response::{Html, IntoResponse},
-	routing::get,
+	routing::{get, get_service},
 	Router, ServiceExt,
 };
 use serde::Deserialize;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 
 // ╾──────────────────────────────╼ MAIN ╾───────────────────────────╼
 #[tokio::main]
 async fn main() {
-	let routes_all = Router::new().merge(routes_hello());
+	let routes_all = Router::new()
+		.merge(routes_hello())
+		.fallback_service(routes_static());
 
 	// ╾──────────────────────────╼ START SERVER ╾───────────────────────╼
 	let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-
 	println!("--> LISTENING on {:?}\n", listener.local_addr());
-
-	axum::serve(listener, routes_hello.into_make_service())
+	axum::serve(listener, routes_all.into_make_service())
 		.await
 		.unwrap();
 
 	// Ok(())
 }
 
-// ____________________________ ROUTES HELLO ____________________________
+// ═══════════════════════════════ ROUTES ════════════════════════════
+fn routes_static() -> Router {
+	Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+// ____________________________ ROUTES_HELLO ____________________________
 fn routes_hello() -> Router {
 	Router::new()
 		.route("/hello", get(handler_hello))
 		.route("/hello2/:name", get(handler_hello2))
 }
 
+// ______________________________________________________________________
 #[derive(Debug, Deserialize)]
 struct HelloParams {
 	name: Option<String>,
