369570ffca19c10ff55eb1a7f3f6ed0de6c2cc09
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-10 15:06:44 +0200

    💫 First Extractor Ctx

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,8 +1,16 @@
-use axum::{body::Body, http::Request, middleware::Next, response::Response};
+use async_trait::async_trait;
+use axum::{
+    body::Body,
+    extract::FromRequestParts,
+    http::{request::Parts, Request},
+    middleware::Next,
+    response::Response,
+    RequestPartsExt,
+};
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
-use crate::web::AUTH_TOKEN;
+use crate::{ctx::Ctx, web::AUTH_TOKEN};
 use crate::{Error, Result};
 
 pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
@@ -18,6 +26,30 @@ pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -
     // TODO: Token components validation.
 
     Ok(next.run(req).await)
+}
+
+// ═══════════════════════════ CTX EXTRACTORS ════════════════════════
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx::from_request_parts", "EXTRACTOR");
+
+        // Use the cookies extractor.
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // Parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token components validation.
+
+        Ok(Ctx::new(user_id))
+    }
 }
 
 /// Parse a token of format `user-[user_id].expiration.[signature]`
