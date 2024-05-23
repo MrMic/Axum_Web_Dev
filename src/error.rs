50868236178f24b7c3e752c41b9c0f56d63a8914
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-23 15:10:53 +0200

    ✨ feat: Error Client & Server

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,13 @@
 use axum::{
-    http::StatusCode,
+    http::{response, StatusCode},
     response::{IntoResponse, Response},
 };
 use serde::Serialize;
 
-// NOTE: ___________________________ VARIABLES ___________________________
+// * NOTE: ___________________________ VARIABLES ___________________________
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
     // Auth errors.
@@ -19,7 +19,6 @@ pub enum Error {
     TicketDeleteFailIdNotFound { id: u64 },
 }
 
-// HACK: _______________________ Error Boilerplate _______________________
 impl std::fmt::Display for Error {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> core::result::Result<(), core::fmt::Error> {
         write!(fmt, "{self:?}")
@@ -33,6 +32,48 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response.
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // ! NOTE: Login
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // ! Auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // ! Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // ! Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
---
+++ structure.md
@@ -0,0 +1,12 @@
+```
+└── 📁src
+    └── ctx.rs
+    └── error.rs
+    └── main.rs
+    └── model.rs
+    └── 📁web
+        └── mod.rs
+        └── mw_auth.rs
+        └── routes_login.rs
+        └── routes_tickets.rs
+```
